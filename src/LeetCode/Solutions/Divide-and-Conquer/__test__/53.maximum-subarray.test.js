a2c1d67c0581e1b05142db17b2d87df8a15febbc
duyue <duyue_i@didiglobal.com>
2019-06-10 18:05:37 +0800

    去除代码，避免使用IDE刷题

--- src/LeetCode/Solutions/Divide-and-Conquer/__test__/53.maximum-subarray.test.js
+++
@@ -1,8 +0,0 @@
-import maxSubArray from '../53.maximum-subarray';
-
-describe('Maximum Subarray', () => {
-  it('test', () => {
-    const arr = [-1, 3, -1, -1, 5];
-    expect(maxSubArray(arr)).toBe(6);
-  });
-});
